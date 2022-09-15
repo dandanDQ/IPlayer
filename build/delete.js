26decbc533ecbcf9fe1f7126aa1261462ea7dc19
dandanDQ <shendanqiong@qq.com>
2022-09-15 21:05:58 +0800

    fix: unpublish some version

---
+++ build/delete.js
@@ -0,0 +1,34 @@
+
+let exec_ = require('child_process').exec
+
+const sleep = async (seconds) => {
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, seconds * 1000)
+  })
+}
+const exec = async (command) => {
+  const res = await new Promise((resolve, reject) => {
+    exec_(command, (err, stdout, stderr) => {
+      console.error(err)
+      console.log(stdout)
+      console.error(stderr)
+      if(err) reject(err)
+
+      resolve({stdout, stderr})
+    })
+  })
+  return res
+}
+
+const main = async () => {
+  for(let i = 6; i <= 88; i += 1) {
+    const version = `1.0.${i}`
+    await exec(`npm unpublish vue-iplayer@${version} --force`)
+    console.log(`npm unpublish vue-iplayer@${version} --force`)
+    await sleep(3)
+  }
+}
+
+main()
